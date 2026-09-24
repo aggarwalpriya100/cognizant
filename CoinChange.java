b7f6fa7790a00b307950e82c4689d74133c34042
aggarwalpriya100 <aggarwalpriya8958@gmail.com>
2026-09-24 14:58:21 +0530

    CoinChange.java

---
+++ CoinChange.java
@@ -0,0 +1,24 @@
+// Coin Change 
+
+
+class Solution {
+	public int coinChange(int[] coins , int amount){
+		int [] dp = new int[amount+1];
+		
+		// Initally , assume impossible 
+		
+		Arrays.fill(dp , amount+1);
+		
+		// O coins needed to make amount 0 
+		
+		dp[0] = 0;
+		for(int i = 1 ; i<= amount ; i++){
+			for(int coin : coins){
+				if(coin <= i){
+					dp[i] = Math.min(dp[i] , dp[i-coin]+1);
+				}
+			}
+		}
+		return dp[amount] > amount ? -1 : dp[amount];
+	}
+}
