3f121971b814789f3a3862e4edca19fce070ec80
aggarwalpriya100 <aggarwalpriya8958@gmail.com>
2026-09-24 16:14:53 +0530

    SubsequencevsSubstring.java

---
+++ SubsequencevsSubstring.java
@@ -0,0 +1,21 @@
+// Subsequence VS Substring
+/* 
+	Substring
+
+		Must be continuous:
+		"abcde"
+		  ↑↑↑
+		 "bcd"
+		
+	Subsequence
+
+		Doesn't need to be continuous:
+			"abcde"
+			 ↑ ↑ ↑
+		     a c e
+			 
+			 
+	So LCS = subsequence, not substring.
+*/ 
+
+	
